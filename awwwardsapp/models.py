689f67ed3881898fa77c359d3e7ae69ac8bf6ed6
Burence1 <burensomondi@gmail.com>
2021-05-28 14:48:49 +0300

    structure database

--- awwwardsapp/models.py
+++ awwwardsapp/models.py
@@ -23,3 +23,27 @@ class Projects(models.Model):
   urls=models.URLField()
   pub_date=models.DateTimeField(auto_now_add=True)
   profile=models.ForeignKey(Profile,on_delete=models.CASCADE)
+
+def __str__(self):
+  return self.name
+
+class Meta:
+  ordering=['-pub_date']
+
+class Ratings(models.Model):
+  design = models.IntegerField(choices=list(zip(range(1, 11), range(1, 11))),default=0)
+  usability = models.IntegerField(choices=list(zip(range(1, 11), range(1, 11))),default=0)
+  content = models.IntegerField(choices=list(zip(range(1, 11), range(1, 11))),default=0)
+  rater = models.ForeignKey(Profile,on_delete=models.CASCADE)
+  projects=models.ForeignKey(Projects,on_delete=models.CASCADE)
+  pub_date=models.DateTimeField(auto_now_add=True)
+  design_average=models.FloatField(default=0)
+  usability_average=models.FloatField(default=0)
+  content_average=models.FloatField(default=0)
+  average_rating=models.FloatField(default=0)
+
+  def __str__(self):
+    return self.projects
+
+  class Meta:
+    ordering=['-pub_date']
