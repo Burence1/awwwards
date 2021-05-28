5534c212b8b2169e1ae17d9f989c7a53559e8203
Burence1 <burensomondi@gmail.com>
2021-05-28 15:02:11 +0300

    add profile and projects db query methods

--- awwwardsapp/models.py
+++ awwwardsapp/models.py
@@ -15,6 +15,11 @@ class Profile(models.Model):
   def __str__(self):
     return self.user.username
 
+  def save_profile(self):
+    self.save()
+  
+  def delete_profile(self):
+    self.delete()
 
 class Projects(models.Model):
   name=models.CharField(max_length=50)
@@ -23,9 +28,36 @@ class Projects(models.Model):
   urls=models.URLField()
   pub_date=models.DateTimeField(auto_now_add=True)
   profile=models.ForeignKey(Profile,on_delete=models.CASCADE)
+  voters = models.IntegerField(default=0)
 
-def __str__(self):
-  return self.name
+
+  def __str__(self):
+    return self.name
+
+  def save_project(self):
+    self.save()
+  
+  def delete_project(self):
+    self.delete()
+
+  def voters_num(self):
+    return self.voters.count()
+
+  @classmethod
+  def get_all_projects(cls):
+    return cls.objects.all()
+
+  @classmethod
+  def get_project(cls,id):
+    return Projects.objects.get(id=id)
+
+  @classmethod
+  def search_project(cls,name):
+    return cls.objects.filter(name__icontains=name)
+
+  @classmethod
+  def user_projects(cls,profile):
+    return cls.objects.filter(profile=profile)  
 
 class Meta:
   ordering=['-pub_date']
@@ -41,9 +73,14 @@ class Ratings(models.Model):
   usability_average=models.FloatField(default=0)
   content_average=models.FloatField(default=0)
   average_rating=models.FloatField(default=0)
-
+  
   def __str__(self):
     return self.projects
+
+  def save_rating(self):
+    self.save()
+  
+  def delet
 
   class Meta:
     ordering=['-pub_date']
