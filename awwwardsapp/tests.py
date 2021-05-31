57bdde0cecea31799eef4ebf57946024b9ab60f3
Burence1 <burensomondi@gmail.com>
2021-06-01 00:38:16 +0300

    add styling

--- awwwardsapp/tests.py
+++ awwwardsapp/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Profile,Projects,Ratings
 
 # Create your tests here.
+class ProjectsClass(TestCase):
+  def setUp(self):
+    self.burens = User(username='burens',email='burensdev@gmail.com',password='lol')
