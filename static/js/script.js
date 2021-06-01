62ef540d4b1139d299f67a74169aa8fdcba7f02b
Burence1 <burensomondi@gmail.com>
2021-06-01 15:25:51 +0300

    deploy

---
+++ static/js/script.js
@@ -0,0 +1,7 @@
+// $(document).on("scroll", function () {
+//   if ($(document).scrollTop() > 50) {
+//     $("#nav").addClass("shrink");
+//   } else {
+//     $("#nav").removeClass("shrink");
+//   }
+// });
